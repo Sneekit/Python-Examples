320b6795dd924bf3fbd9fb6aebaabc57f074293d
Sneekit <44178182+Sneekit@users.noreply.github.com>
2020-11-30 14:40:39 -0700

    Create exceltocsv.py

---
+++ exceltocsv.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import sys
+import os
+
+infile = sys.argv[1]
+
+# get delim from arguments
+if len(sys.argv)>2:
+        delim = sys.argv[2]
+else:
+        delim=""
+
+# set ending extension and delimiter based on inputs
+endextension=".csv"
+if delim == "":
+        delim = ","
+        endextension=".csv"
+if delim == "tab" or delim == "TAB":
+        delim = "\t"
+        endextension=".tab"
+
+# determine base file names
+basename = os.path.basename(infile)
+filearray = os.path.splitext(infile)
+extension = filearray[1]
+extensionlist = ['.xlsx','.xls']
+
+if infile == "":
+        print("You must provide a file to convert.")
+        sys.exit(1)
+
+if os.path.exists(infile) == False:
+        print("The specified file does not exist.")
+        sys.exit(1)
+
+if extension not in extensionlist:
+        print("You must provide a valid excel file.")
+        sys.exit(1)
+
+# read in xlsx file
+data_xls = pd.read_excel(infile, sheet_name=None)
+
+# export a csv for every sheet if on python 2.7 and above otherwise single sheet only
+sheetnum = 0
+try:
+        for name,sheet in data_xls.items():
+                sheetnum += 1
+                outfile = filearray[0]+"_sheet"+str(sheetnum)+endextension
+                single_sheet = pd.read_excel(infile, sheet_name=name)
+                single_sheet.to_csv(outfile)
+except AttributeError:
+        data_xls.to_csv(filearray[0]+"_sheet1"+endextension,sep=delim)
+
+print(basename+" exported to csv.")
+sys.exit(0)
