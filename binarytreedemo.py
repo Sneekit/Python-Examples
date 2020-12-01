3e998a07cdf7150e21b2154a1273dd8e7c1b18c3
Sneekit <44178182+Sneekit@users.noreply.github.com>
2020-12-01 14:16:44 -0700

    Update binarytreedemo.py

--- binarytreedemo.py
+++ binarytreedemo.py
@@ -4,12 +4,12 @@
 # original tree structure
 #
 #          1
-#			     ^
-#				2     3
-#				^	    ^
-#			 4 5   6 7
-#			 ^     ^
-#			8     9 10
+#          ^
+#       2     3
+#       ^     ^
+#      4 5   6 7
+#      ^     ^
+#     8     9 10
 
 import binarytree as bt
 
